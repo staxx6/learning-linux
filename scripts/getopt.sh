fd3bf691b3a029462e2c91019dad99a5443fdd01
Simon Oczadly <staxx@hotmail.de>
2018-11-12 13:33:28 +0100

    update

--- scripts/getopt.sh
+++ scripts/getopt.sh
@@ -2,7 +2,7 @@
 
 set -o nounset
 
-TEMP=$(getopt -o ve::a: --long verbose, encrypt::,append: \
+TEMP=$(getopt -o ve::a: --long verbose,encrypt::,append: \
     -n $(basename $0) -- "$@") || exit 1
 echo "TEMP=$TEMP"
 eval set -- "$TEMP"
@@ -22,4 +22,27 @@ do
         -e|--encrypt)
             encrypt=1
             cipher="$2"
-            [ -z "$cipher" ] &&
+            [ -z "$cipher" ] && cipher=DES
+            shift 2;;
+        -a|--append)
+            append=1
+            outfile="$2"
+            shift 2;;
+        --)
+            shift
+            break;;
+        *)
+            echo "$(basename $0): Internal error," \
+                "unexpected option $1" >&2
+            exit 2;;
+    esac
+done
+
+echo "verbose=$verbose encrypt=$encrypt cipher=\"$cipher\"" \
+    "append=$append outfile=\"$outfile\""
+
+while [ $# -gt 0 ]
+do
+    echo "Argument: \"$1\""
+    shift
+done
